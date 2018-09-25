42301affd0d5ef0d3cb16f10b46b269607f4e6ec
Ryoung27 <RRYoung89@gmail.com>
2018-09-24 19:22:49 -0500

    Stanford iOS lecture 1 calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var Display: UILabel?
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = Display!.text!
+            Display!.text = textCurrentlyInDisplay + digit
+        } else {
+            Display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
-
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                Display!.text = String(Double.pi)
+            case "√":
+                let operand = Double(Display!.text!)!
+                Display!.text = String(sqrt(operand))
+            default:
+                break
+            }
+        }
+    }
 }
 
